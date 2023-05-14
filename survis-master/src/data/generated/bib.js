091eae523d011aeb507704e3251f6dc1d92a429d
Uwei <67782032+Uwei-UoN@users.noreply.github.com>
2023-05-14 23:57:02 +0100

    Add files via upload

---
+++ survis-master/src/data/generated/bib.js
@@ -0,0 +1,49 @@
+﻿const generatedBibEntries = {
+    "10.1007/978-3-319-60131-1_26": {
+        "abstract": "\"While some public transit data publishers only provide a data dump -- which only few reusers can afford to integrate within their applications -- others provide a use case limiting origin-destination route planning api. The Linked Connections framework instead introduces a hypermedia api, over which the extendable base route planning algorithm ``Connections Scan Algorithm'' can be implemented. We compare the cpu usage and query execution time of a traditional server-side route planner with the cpu time and query execution time of a Linked Connections interface by evaluating query mixes with increasing load. We found that, at the expense of a higher bandwidth consumption, more queries can be answered using the same hardware with the Linked Connections server interface than with an origin-destination api, thanks to an average cache hit rate of 78{\\%}. The findings from this research show a cost-efficient way of publishing transport data that can bring federated public transit route planning at the fingertips of anyone.\",",
+        "address": "\"Cham\",",
+        "author": "\"Colpaert, Pieter and Verborgh, Ruben and Mannens, Erik\",",
+        "booktitle": "\"Web Engineering\",",
+        "editor": "\"Cabot, Jordi and De Virgilio, Roberto and Torlone, Riccardo\",",
+        "isbn": "\"978-3-319-60131-1\"",
+        "pages": "\"403--411\",",
+        "publisher": "\"Springer International Publishing\",",
+        "title": "\"Public Transit Route Planning Through Lightweight Linked Data Interfaces\",",
+        "type": "article",
+        "year": "\"2017\","
+    },
+    "Beck2016Visual": {
+        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
+        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
+        "doi": "10.1109/TVCG.2015.2467757",
+        "journal": "IEEE Transactions on Visualization and Computer Graphics",
+        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
+        "number": "01",
+        "publisher": "IEEE",
+        "series": "TVCG",
+        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+        "type": "article",
+        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
+        "volume": "22",
+        "year": "2016"
+    },
+    "inbook": {
+        "author": "Toutouh, Jamal and Lebrus\u00e1n Murillo, Irene and Cintrano, Christian",
+        "doi": "10.1007/978-3-030-96753-6_16",
+        "isbn": "978-3-030-96752-9",
+        "month": "01",
+        "pages": "223-239",
+        "title": "Using Open Data to Analyze Public Bus Service from an Age Perspective: Melilla Case",
+        "type": "inbook",
+        "year": "2022"
+    },
+    "lehoux2020faster": {
+        "abstract": "",
+        "author": "Lehoux, Vassilissa and Loiodice, Christelle",
+        "booktitle": "20th Symposium on Algorithmic Approaches for Transportation Modelling, Optimization, and Systems (ATMOS 2020)",
+        "organization": "Schloss Dagstuhl-Leibniz-Zentrum f{\\\"u}r Informatik",
+        "title": "Faster Preprocessing for the Trip-Based Public Transit Routing Algorithm",
+        "type": "inproceedings",
+        "year": "2020"
+    }
+};
